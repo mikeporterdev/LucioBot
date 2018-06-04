4ae550c7e552567f80549fc83be15eb9a7a3e378
Michael Porter <m_porter@live.co.uk>
2018-06-04 23:08:13 +0100

    Added basic discord bot with simple command

--- __main__.py
+++ __main__.py
@@ -1,2 +1,24 @@
-def __main__():
-    print("Test for TeamCity2")
+import os
+
+from discord.ext.commands import Bot
+
+BOT_PREFIX = ("Lucio ", "lucio ")
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
+
+client.run(os.environ.get('DISCORD_KEY'))
---
+++ conf.py
@@ -0,0 +1,6 @@
+conf = {
+    "DB_HOST": "localhost",
+    "DB_SCHEMA": "LucioBot",
+    "DB_USER": "postgres",
+    "DB_PASSWORD": "postgres",
+}
